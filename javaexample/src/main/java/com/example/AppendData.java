ac3a52786d5103c992a913d69a79ee6c620c3ae6
Shanubrata Howlader <shanubrata.howlader@gmail.com>
2021-05-09 12:03:25 +0530

    Java test commit with Java module.

---
+++ javaexample/src/main/java/com/example/AppendData.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class AppendData {
+
+	public static String AppendNmae(String Name) {
+		return "Hello " + Name;
+	}
+	public   String AppendFullNmae(String fName, String lName) {
+		return "Hello " + fName + " "+ lName;
+	}
+}
